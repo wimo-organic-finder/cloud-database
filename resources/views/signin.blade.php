fd821157e71f54f944314717cd9890f9030e440f
aniscoquelet <anis.coquelet@gmail.com>
2019-10-24 14:13:52 +0200

    Nassim la pute

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -11,8 +11,7 @@
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <script type="text/javascript" src="js/bootstrap.js">
 
 </script>
@@ -35,7 +34,7 @@
       }
     </style>
     <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet" type="text/css" rel="stylesheet" type="text/css">
+    <link href="css/signin.css" rel="stylesheet">
   </head>
   <body class="text-center">
     <form class="form-signin">
--- views/register.blade.php
+++ views/register.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.5">
-    <title>Signin Template · Bootstrap</title>
+    <title>Inscription</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
 
@@ -40,17 +40,34 @@
   <body class="text-center">
     <form class="form-signin">
   <img class="mb-4" src="cloud-logo.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+  <h1 class="h3 mb-3 font-weight-normal">Inscription</h1>
+  <label for="inputLastName" class="sr-only">Nom</label>
+  <input type="text" id="inputLastName" class="form-control" placeholder="Nom" required autofocus>
+  <label for="inputFirstName" class="sr-only">Prénom</label>
+  <input type="text" id="inputFirstName" class="form-control" placeholder="Prénom" required>
   <label for="inputEmail" class="sr-only">Addresse e-mail</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Addresse e-mail" required autofocus>
   <label for="inputPassword" class="sr-only">Mot de passe</label>
   <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Se souvenir de moi
-    </label>
+  <label for="inputPasswordConfirm" class="sr-only">Mot de passe</label>
+  <input type="password" id="inputPasswordConfirm" class="form-control" placeholder="Confirmer mot de passe" required>
+
+  <label for="mobilePhone" class="sr-only">Numéro de téléphone</label>
+  <input type="tel" id="mobilePhone" class="form-control" placeholder="Num. tél." required autofocus>
+  <label for="dateNaiss" class="sr-only">Date de naissance</label>
+  <input type="date" id="dateNaiss" class="form-control" placeholder="Date de naissance" required autofocus>
+  <div class="custom-control custom-radio custom-control-inline">
+    <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+    <label class="custom-control-label" for="customRadioInline1">Homme</label>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <div class="custom-control custom-radio custom-control-inline">
+    <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+    <label class="custom-control-label" for="customRadioInline2">Femme</label>
+  </div>
+  <div class="checkbox mb-3">
+
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">S'inscrire</button>
 
 </form>
 </body>
